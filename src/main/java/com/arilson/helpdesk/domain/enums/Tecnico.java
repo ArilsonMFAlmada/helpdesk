da7503caab643a3ebe181156ef1bbed697027fc7
Arilson Almada <arilson.almada@gmail.com>
2021-10-10 16:58:00 -0700

    Classes de domínios iniciadas

---
+++ src/main/java/com/arilson/helpdesk/domain/enums/Tecnico.java
@@ -0,0 +1,18 @@
+package com.arilson.helpdesk.domain.enums;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Tecnico extends Pessoa{
+
+    private List<Chamado> chamados = new ArrayList<>();
+
+    public Tecnico() {
+    }
+
+    public Tecnico(Integer id, String nome, String cpf, String email, String senha) {
+        super(id, nome, cpf, email, senha);
+    }
+
+
+}
