e793bdce5de6b397d6dc0a866b9bfa7edf197122
Arilson Almada <arilson.almada@gmail.com>
2021-10-11 03:15:37 -0700

    Anotações JPA para classes de dominio

--- src/main/java/com/arilson/helpdesk/domain/enums/Tecnico.java
+++ src/main/java/com/arilson/helpdesk/domain/enums/Tecnico.java
@@ -1,17 +1,24 @@
 package com.arilson.helpdesk.domain.enums;
 
+import javax.persistence.Entity;
+import javax.persistence.OneToMany;
 import java.util.ArrayList;
 import java.util.List;
 
+@Entity
 public class Tecnico extends Pessoa{
 
+    @OneToMany(mappedBy = "tecnico")
     private List<Chamado> chamados = new ArrayList<>();
 
     public Tecnico() {
+        super();
+        addPerfil(Perfil.TECNICO);
     }
 
     public Tecnico(Integer id, String nome, String cpf, String email, String senha) {
         super(id, nome, cpf, email, senha);
+        addPerfil(Perfil.TECNICO);
     }
 
 
