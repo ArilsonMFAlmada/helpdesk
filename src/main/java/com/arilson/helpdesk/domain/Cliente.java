870a0f1e9f9d94d480dbbf5f881d66e035260d67
Arilson Almada <arilson.almada@gmail.com>
2021-10-18 14:29:40 -0700

    CRUD para cliente implementado

--- src/main/java/com/arilson/helpdesk/domain/Cliente.java
+++ src/main/java/com/arilson/helpdesk/domain/Cliente.java
@@ -1,5 +1,7 @@
 package com.arilson.helpdesk.domain;
 
+import com.arilson.helpdesk.domain.dtos.ClienteDTO;
+import com.arilson.helpdesk.domain.dtos.TecnicoDTO;
 import com.arilson.helpdesk.domain.enums.Perfil;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
@@ -7,6 +9,7 @@ import javax.persistence.Entity;
 import javax.persistence.OneToMany;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Entity
 public class Cliente extends Pessoa{
@@ -19,6 +22,17 @@ public class Cliente extends Pessoa{
         super();
         addPerfil(Perfil.CLIENTE);
     }
+
+    public Cliente(ClienteDTO obj) {
+        this.id = obj.getId();
+        this.nome = obj.getNome();
+        this.cpf = obj.getCpf();
+        this.email = obj.getEmail();
+        this.senha = obj.getSenha();
+        this.perfis = obj.getPerfis().stream().map(x -> x.getCodigo()).collect(Collectors.toSet());
+        this.dataCriacao = obj.getDataCriacao();
+    }
+
 
     public Cliente(Integer id, String nome, String cpf, String email, String senha) {
         super(id, nome, cpf, email, senha);
